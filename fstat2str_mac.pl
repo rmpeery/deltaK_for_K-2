6a8fa28344f9937af53ae968d63bdf4e4d6a75ae
rmpeery <rmpeery@gmail.com>
2019-10-16 10:40:00 -0600

    Update fstat2str_mac.pl

--- fstat2str_mac.pl
+++ fstat2str_mac.pl
@@ -8,7 +8,8 @@ closedir(DIR);
 
 my $file = ();
 foreach $file (@files) {
-	my $cmd = "/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Commands/java -Xmx1024m -Xms512m -jar /Users/rhiannonpeery/Documents/sidePieces/K2/PGDSpider_2.1.1.5/PGDSpider2-cli.jar -inputfile $file -inputformat FSTAT -outputfile $file.str -outputformat STRUCTURE -SPID /Users/rhiannonpeery/Documents/sidePieces/K2/PGDSpider_2.1.1.5/fstat2str.spid";
+#commands need to be modified for paths to java, in, out, SPID files
+	my $cmd = "/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Commands/java -Xmx1024m -Xms512m -jar PGDSpider2-cli.jar -inputfile $file -inputformat FSTAT -outputfile $file.str -outputformat STRUCTURE -SPID fstat2str.spid";
 	system($cmd);
 	}
 print "done\n";
